6096706827f5dd31902a29c1487da1a00d70cc69
Adam Suskin <5596609+adamsuskin@users.noreply.github.com>
2025-10-25 23:53:04 -0400

    add max-turns

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,66 @@
 #!/usr/bin/env node
-import { Args, Command } from "@effect/cli";
+import { Args, Command, Options } from "@effect/cli";
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
-import { Console, Duration, Effect, Ref, Schedule } from "effect";
+import { Console, Duration, Effect, Option, Ref, Schedule } from "effect";
 import { Main } from "./main.js";
 import { MacchioService } from "./macchio-service.js";
 import { BottomBar } from "./bottom-bar.js";
 
 const prompt = Args.text({
   name: "prompt",
-});
+}).pipe(Args.withDescription("the prompt to run"));
 
-const program = (prompt: string) =>
+const maxTurns = Options.integer("max-turns").pipe(
+  Options.withAlias("M"),
+  Options.withDescription("the maximum number of turns to run"),
+  Options.optional
+);
+
+const program = ({
+  maxTurns,
+  prompt,
+}: {
+  maxTurns: Option.Option<number>;
+  prompt: string;
+}) =>
   Effect.gen(function* () {
     const bottomBar = yield* Effect.acquireRelease(
       Effect.succeed(new BottomBar("Macchio v1.0 - use ctrl+c to exit")),
       (bottomBar) =>
         Console.log("Cleaning up bottom bar").pipe(
-          Effect.andThen(() => bottomBar.cleanup())
+          Effect.andThen(() => {
+            bottomBar.disable();
+            return Effect.void;
+          })
         )
     );
     bottomBar.enable();
 
     const turnRef = yield* Ref.make(1);
 
-    yield* Effect.repeat(
-      Effect.gen(function* () {
-        const turn = yield* Ref.get(turnRef);
-        bottomBar.updateText(
-          `Macchio v1.0 - use ctrl+c to exit - turn ${turn}`
-        );
-        yield* Main.run(prompt);
-        yield* Ref.update(turnRef, (turn) => turn + 1);
-      }),
-      Schedule.spaced(Duration.millis(100))
-    );
+    const turnEffect = Effect.gen(function* () {
+      const turn = yield* Ref.get(turnRef);
+      bottomBar.updateText(`Macchio v1.0 - use ctrl+c to exit - turn ${turn}`);
+      yield* Main.run(prompt);
+      yield* Ref.update(turnRef, (turn) => turn + 1);
+    });
+
+    const hasMaxTurns = maxTurns.pipe(Option.isSome);
+    if (!hasMaxTurns) {
+      yield* Effect.repeat(turnEffect, Schedule.spaced(Duration.millis(200)));
+    }
+
+    yield* Effect.repeatN(turnEffect, maxTurns.pipe(Option.getOrThrow) - 1);
+    bottomBar.disable();
   }).pipe(
     Effect.scoped,
     Effect.onInterrupt(() => Main.cleanup())
   );
 
-const command = Command.make("macchio", { prompt }, ({ prompt }) =>
-  program(prompt)
+const command = Command.make(
+  "macchio",
+  { maxTurns, prompt },
+  ({ maxTurns, prompt }) => program({ maxTurns, prompt })
 );
 
 // Set up the CLI application
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { Console, Duration, Effect, Layer, Schedule } from "effect";
+import { Console, Duration, Effect, Layer } from "effect";
 
 import { MacchioService } from "./macchio-service.js";
 
 export class Main extends Effect.Service<Main>()("Main", {
   succeed: {
     run: (prompt: string) =>
-      Effect.repeat(
-        MacchioService.singleTurn(prompt),
-        Schedule.spaced(Duration.millis(100))
-      ).pipe(Effect.asVoid),
+      MacchioService.singleTurn(prompt).pipe(Effect.asVoid),
     cleanup: MacchioService.cleanup,
   },
   dependencies: [MacchioService.Default],
